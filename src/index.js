29a83fbecaa6be9e308c85519fc6d8ce3ec17c7c
Saidazizkhon Akbarov <akbaroff5@gmail.com>
2020-10-22 11:13:40 +0500

    init: in works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let currentChar,
+        lastChar,
+        stack = [];
+    for (let i = 0; i < str.length; i++) {
+        currentChar = str[i];
+        for (let k = 0; k < bracketsConfig.length; k++) {
+            if (bracketsConfig[k][0] === currentChar) {
+                stack.push(currentChar);
+            } else if (bracketsConfig[k][1] === currentChar) {
+                if (stack.length > 0) {
+                    lastChar = stack[stack.length - 1];
+                    if (currentChar === bracketsConfig[k][1] && lastChar === bracketsConfig[k][0]) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+    }
+    console.log(stack.length);
+    return stack.length === 0;
 }
