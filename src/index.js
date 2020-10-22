665595521d573c34be48e0d72703d81c2ecb3cbe
Saidazizkhon Akbarov <akbaroff5@gmail.com>
2020-10-22 13:43:48 +0500

    fix: now works correctly!

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ module.exports = function check(str, bracketsConfig) {
     for (let i = 0; i < str.length; i++) {
         currentChar = str[i];
         for (let k = 0; k < bracketsConfig.length; k++) {
+            if (bracketsConfig[k][0] === bracketsConfig[k][1] && currentChar === bracketsConfig[k][0]) {
+                if (stack.length > 0) {
+                    lastChar = stack[stack.length - 1];
+                    if (currentChar === lastChar) {
+                        stack.pop();
+                        break;
+                    }
+                }
+            }
             if (bracketsConfig[k][0] === currentChar) {
                 stack.push(currentChar);
             } else if (bracketsConfig[k][1] === currentChar) {
@@ -13,10 +22,11 @@ module.exports = function check(str, bracketsConfig) {
                     if (currentChar === bracketsConfig[k][1] && lastChar === bracketsConfig[k][0]) {
                         stack.pop();
                     }
+                } else if (bracketsConfig[k][0] != bracketsConfig[k][1]) {
+                    return false;
                 }
             }
         }
     }
-    console.log(stack.length);
     return stack.length === 0;
 }
